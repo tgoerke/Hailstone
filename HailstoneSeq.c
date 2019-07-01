a1fba84eb4267985ec039f0e04b8a0b58c10f337
T Goerke <t.goerke@mailbox.org>
2019-07-01 18:42:05 +0200

    first commit

---
+++ HailstoneSeq.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int Hailstone (int n)
+{
+  if (n % 2 == 0) {
+    return n /= 2;
+  }
+  else {
+    return n = 3 * n + 1;
+  }
+  return n;
+}
+
+void main() {
+  int n=7; while(n!=1) {
+    n = Hailstone(n);
+    printf("%i\n", n);
+  }
+}
+
+
---
+++ HailstoneSeq.ino
@@ -0,0 +1,25 @@
+int Hailstone (int n)
+{
+  if (n % 2 == 0) {
+    return n /= 2;
+  }
+  else {
+    return n = 3 * n + 1;
+  }
+  return n;
+}
+
+void setup() {
+  // put your setup code here, to run once:
+   Serial.begin(9600);
+}
+
+void loop() {
+  int n=100; while(n!=1) {
+    n = Hailstone(n);
+    Serial.println(n);
+    delay(100);
+  }
+}
+
+
